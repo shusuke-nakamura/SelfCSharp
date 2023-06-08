0d6f4fe0fd62d14e09cf39432185933747f00aca
Shusuke Nakamura <shusuke.pana@gmail.com>
2023-06-08 16:48:16 +0900

    リスト1.2まで学習完了

--- Chap01/Person.cs
+++ Chap01/Person.cs
@@ -1,4 +1,39 @@
 namespace SelfCSharp.Chap01
 {
+    /// <summary>
+    /// 人物の情報を表す
+    /// </summary>
+    internal class Person
+    {
+        /// <summary>
+        /// 人物の名前
+        /// </summary>
+        public string? firstName;
 
+        /// <summary>
+        /// 人物の苗字
+        /// </summary>
+        public string? lastName;
+
+        /// <summary>
+        /// 氏名を表示する
+        /// </summary>
+        /// <param name="Age">年齢</param>
+        /// <returns>氏名と年齢に基づいて整形された文字列</returns>
+        public string show(int Age)
+        {
+            return $"名前は{lastName}{firstName}、{Age}歳です。";
+        }
+    }
+
+    internal class PersonClient
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person();
+            p.firstName = "太郎";
+            p.lastName = "山田";
+            Console.WriteLine(p.show(20));
+        }
+    }
 }
