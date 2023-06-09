e25b11c08b1989923f2c89179f270949a5486c7f
Shusuke Nakamura <shusuke.pana@gmail.com>
2023-06-10 00:38:57 +0900

    リスト2.6まで学習完了

---
+++ Chap02/Parse.cs
@@ -0,0 +1,14 @@
+namespace SelfCSharp.Chap02
+{
+    internal class Parse
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Int32.Parse("108"));
+            Console.WriteLine(Single.Parse("0.1413"));
+            Console.WriteLine(Int32.Parse("FF", System.Globalization.NumberStyles.HexNumber));
+            Console.WriteLine(Double.Parse("0.6532e2"));
+            Console.WriteLine(Convert.ToInt32("1010", 2));
+        }
+    }
+}
