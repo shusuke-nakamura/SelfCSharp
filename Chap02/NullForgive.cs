1a873efb7bbc8b19e21bb7b6bd06228b47764f91
Shusuke Nakamura <shusuke.pana@gmail.com>
2023-06-16 22:03:56 +0900

    リスト2.11まで学習完了

---
+++ Chap02/NullForgive.cs
@@ -0,0 +1,12 @@
+namespace SelfCSharp.Chap02
+{
+    internal class NullForgive
+    {
+        static void Main(string[] args)
+        {
+            string?[] format = { "2022-12-25 10:16:23" };
+            var dt = DateTime.Parse(format[0]!);
+            Console.WriteLine(dt);
+        }
+    }
+}
