adb2887599d37600568596d6b768b58d05b72d6b
Shusuke Nakamura <shusuke.pana@gmail.com>
2023-06-17 16:02:36 +0900

    リスト2.13 の学習完了

---
+++ Chap02/ArrayMulti.cs
@@ -0,0 +1,19 @@
+namespace SelfCSharp.Chap02
+{
+    internal class ArrayMulti
+    {
+        static void Main(string[] args)
+        {
+            // int[,] data = {
+            //     {10, 11, 12},
+            //     {20, 21, 22}
+            // };
+            var data = new[,]
+            {
+                {10, 11, 12},
+                {20, 21, 22}
+            };
+            Console.WriteLine(data[0, 1]);
+        }
+    }
+}
