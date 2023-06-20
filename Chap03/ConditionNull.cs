7164c95e67d4de5545e2f0ef62cb4dd5e4851c5c
Shusuke Nakamura <shusuke.pana@gmail.com>
2023-06-20 15:59:57 +0900

    3.3 関係演算子の学習完了

---
+++ Chap03/ConditionNull.cs
@@ -0,0 +1,12 @@
+namespace SelfCSharp.Chap03
+{
+    internal class ConditionNull
+    {
+        static void Main(string[] args)
+        {
+            string? str = "山田";
+            string def = "権兵衛";
+            Console.WriteLine(str ?? def);
+        }
+    }
+}
