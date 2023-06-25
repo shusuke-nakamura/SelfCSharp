ff37b658b582f21a938cf7ca30d308373a15716c
Shusuke Nakamura <shusuke.pana@gmail.com>
2023-06-25 22:59:43 +0900

    4.3 ループの制御まで学習完了

---
+++ Chap04/practice/PContinue.cs
@@ -0,0 +1,22 @@
+namespace SelfCSharp.Chap04.practice
+{
+    internal class PContinue
+    {
+        static void Main(string[] args)
+        {
+            var i = 0;
+            var sum = 0;
+
+            while (i < 100)
+            {
+                i++;
+                if (i % 2 != 0)
+                {
+                    continue;
+                }
+                sum += i;
+            }
+            Console.WriteLine($"合計値は{sum}です。");
+        }
+    }
+}
