05cd63596730289a1724c9e5f7916b0e9bf409e6
Shusuke Nakamura <shusuke.pana@gmail.com>
2023-06-28 22:38:13 +0900

    5.1 文字列の操作の学習完了

---
+++ Chap05/practice/PSubstr.cs
@@ -0,0 +1,11 @@
+namespace SelfCSharp.Chap05.practice
+{
+    internal class PSubstr
+    {
+        static void Main(string[] args)
+        {
+            var str = "プログラミング言語";
+            Console.WriteLine(str.Substring(4, 3));
+        }
+    }
+}
